98452470c48a3499834e8b74851e532f7675ccb4
Danilo <levitano@hotmail.com>
2022-12-13 12:22:32 -0300

    fix db_port in server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from "./app";
 import "dotenv/config"
 import { startDatabase } from "./database";
 
-const PORT = process.env.port || 3000
+const PORT = process.env.DB_PORT
 
 export default app.listen(PORT, async () => {
     await startDatabase()
